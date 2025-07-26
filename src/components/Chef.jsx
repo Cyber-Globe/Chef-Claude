fabb213a70d15634909c0481ae1474e8e0834e68
cyber <ojeikelvin@gmail.com>
2025-07-26 08:45:27 +0100

    worked on the partial form handler

--- src/components/Chef.jsx
+++ src/components/Chef.jsx
@@ -1,14 +1,27 @@
 function Chef() {
+  const ingredients = ["Chicken", "Oregano", "Tomatoes"];
+  const ingredientElement = ingredients.map((ingredient) => {
+    return <li key={ingredient}>{ingredient}</li>;
+  });
+  function submitted(event) {
+    event.preventDefault();
+    console.log("Form submitted!");
+    const formData = new FormData(event.target);
+    const ingredient = formData.get("ingredient");
+    console.log("Ingredient added: ", ingredient);
+  }
   return (
-    <main>
-      <form action="" className="add-ingredient-form">
+    <main className="main">
+      <form action="" className="add-ingredient-form" onSubmit={submitted}>
         <input
           type="text"
           aria-label="add ingredient"
           placeholder="e.g. oregano"
+          name="ingredient"
         />
         <button>Add Ingredient</button>
       </form>
+      <ul>{ingredientElement}</ul>
     </main>
   );
 }
